32f705e8350837eceff26242706fdb9c99b11565
Juanelc197 <juanelc197@outlook.com>
2017-06-27 18:38:54 -0500

    Proyecto

---
+++ app/src/main/java/com/example/juanestebanlc/gginveproyecto/TerniumActivity.java
@@ -0,0 +1,13 @@
+package com.example.juanestebanlc.gginveproyecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TerniumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ternium);
+    }
+}
